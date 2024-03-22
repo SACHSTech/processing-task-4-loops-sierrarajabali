4cf6af710872d44624acbfd2fb0747689c8fce01
sierrarajabali <sierra.rajabali26@ycdsbk12.ca>
2024-03-21 22:38:33 -0400

    progress

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255,255,255);
   }
 
   /**
@@ -25,11 +25,38 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    for(int lineX = 15; lineX <= 200; lineX += 15){
+      stroke(0,0,0);
+      line(lineX, 0, lineX, 200);
+    }
+    for(int lineY = 15; lineY <= 200; lineY += 15){
+      stroke(0,0,0);
+      line(0, lineY, 200, lineY);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    for (int circleY = 30; circleY <= 200; circleY += 35) {
+      for (int circleX = 230; circleX <= 400; circleX += 35) {
+        fill(212, 15, 125);
+        ellipse(circleX, circleY, 20, 20);
+      }
+    }
+
+    double colours = 0; 
+    for(int lineX = 0; lineX <= 200; lineX ++){
+      line(lineX, 200, lineX, 400);
+      colours += 1.25;
+      stroke((int)colours, (int)colours, (int)colours);
+
+    }
+
+    fill(225, 100, 0);
+    stroke(0, 0, 0);
+    ellipse(300, 300, 20, 125);
+
+    fill(37, 108, 45);
+    stroke(37, 108, 45);
+    ellipse(300, 300, 30, 30);
+  
   }
   
   // define other methods down here.
